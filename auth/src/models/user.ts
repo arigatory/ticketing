9e6d30bdbd4502a3a2a6f2e05c680049720dec60
Ivan Panchenko <arigatory@gmail.com>
2024-12-09 02:58:29 +0300

    feat: add hash

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import { Password } from "../services/password";
 
 // An interface that descripes the props required to create new User
 interface UserAttrs {
@@ -8,15 +9,14 @@ interface UserAttrs {
 
 // An interface that describes the props User model has
 interface UserModel extends mongoose.Model<UserDoc> {
-    build(attrs: UserAttrs): UserDoc
+  build(attrs: UserAttrs): UserDoc;
 }
 
 // single
 interface UserDoc extends mongoose.Document {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
-
 
 const userSchema = new mongoose.Schema({
   email: {
@@ -28,6 +28,15 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.pre("save", async function (done) {
+  if (this.isModified("password")) {
+    const hashed = await Password.toHash(this.get("password"));
+    this.set("password", hashed);
+    done();
+  }
+});
+
 userSchema.statics.build = (attrs: UserAttrs) => {
   return new User(attrs);
 };
