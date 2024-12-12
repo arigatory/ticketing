508fef031d254748c5b2e098b554bb3ef4a6dda8
Ivan Panchenko <arigatory@gmail.com>
2024-12-12 03:43:24 +0300

    use auth strategy

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ticketing
+
+
+kubectl create secret generic jwt-secret --from-literal=JWT_KEY=asdf
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,8 @@ import express from "express";
 import { json } from "body-parser";
 import "express-async-errors";
 import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
@@ -10,7 +12,14 @@ import { errorHandler } from "./middlewares/error-handler";
 import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
+app.set("trust proxy", true);
 app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -24,11 +33,13 @@ app.all("*", async () => {
 app.use(errorHandler);
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
   try {
     await mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
-    console.log('Connected to MongoDb');
-  } catch (error) {
-  }
+    console.log("Connected to MongoDb");
+  } catch (error) {}
 
   app.listen(3000, () => {
     console.log("Listening on port 3000!");
