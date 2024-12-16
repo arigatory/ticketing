7e5f098ae7251d7d931a6f7eca982d710123cdf2
Ivan Panchenko <arigatory@gmail.com>
2024-12-17 00:18:33 +0300

    test

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,11 +1,16 @@
-import { MongoMemoryServer } from "mongodb-memory-server";
-import mongoose from "mongoose";
-import { app } from "../app";
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+import { app } from '../app';
+import request from 'supertest';
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 let mongo: MongoMemoryServer;
 
 beforeAll(async () => {
-  process.env.JWT_KEY = "random_string";
+  process.env.JWT_KEY = 'random_string';
   mongo = await MongoMemoryServer.create();
   const montoUri = mongo.getUri();
 
@@ -28,3 +33,20 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get('Set-Cookie');
+
+  if (!cookie) {
+    throw new Error('Failed to get cookie from response');
+  }
+  return cookie;
+};
