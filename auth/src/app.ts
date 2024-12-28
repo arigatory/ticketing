91b6843772fe9aac65d885d1b57010e12f843bfb
Ivan Panchenko <arigatory@gmail.com>
2024-12-28 03:58:14 +0300

    fix: typing

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import { json } from 'body-parser';
+import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@arigatory-tickets/common';
 
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
-import { errorHandler, NotFoundError } from '@arigatory-tickets/common';
 
 const app = express();
 app.set('trust proxy', true);
@@ -15,7 +15,7 @@ app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test',
+    secure: process.env.NODE_ENV !== 'test'
   })
 );
 
@@ -24,10 +24,12 @@ app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
 
-app.all('*', async () => {
+app.all('*', async (req: Request, res: Response) => {
   throw new NotFoundError();
 });
 
-app.use(errorHandler);
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  errorHandler(err, req, res, next);
+});
 
 export { app };
