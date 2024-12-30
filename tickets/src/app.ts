ce1d81bd3d287fb015e6841bc780e6b3103c7839
Ivan Panchenko <arigatory@gmail.com>
2024-12-30 17:14:37 +0300

    feat: add tickets

---
+++ tickets/src/app.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import { errorHandler, NotFoundError } from '@arigatory-tickets/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+  })
+);
+
+
+app.all('*', async (req: Request, res: Response) => {
+  throw new NotFoundError();
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  errorHandler(err, req, res, next);
+});
+
+export { app };
