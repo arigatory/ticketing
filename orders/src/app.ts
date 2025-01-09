96e58b55da1ab3ce0d416c355c064aa604e19237
Ivan Panchenko <arigatory@gmail.com>
2025-01-09 15:02:53 +0300

    add orders service

---
+++ orders/src/app.ts
@@ -0,0 +1,39 @@
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@arigatory-tickets/common';
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes';
+import { updateTicketRouter } from './routes/update';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+
+app.use(createTicketRouter);
+app.use(indexTicketRouter);
+app.use(showTicketRouter);
+app.use(updateTicketRouter);
+
+app.all('*', async (req: Request, res: Response) => {
+  throw new NotFoundError();
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  errorHandler(err, req, res, next);
+});
+
+export { app };
