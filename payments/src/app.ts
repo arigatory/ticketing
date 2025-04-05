b73f68c6494a8bac1e7e55c7cdae8c9450caaf02
Ivan Panchenko <arigatory@gmail.com>
2025-04-05 11:44:18 +0300

    feat: add payments initial setup

---
+++ payments/src/app.ts
@@ -0,0 +1,32 @@
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+import {
+  errorHandler,
+  NotFoundError,
+  currentUser,
+} from '@arigatory-tickets/common';
+
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+
+
+app.all('*', async (req: Request, res: Response) => {
+  throw new NotFoundError();
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  errorHandler(err, req, res, next);
+});
+
+export { app };
